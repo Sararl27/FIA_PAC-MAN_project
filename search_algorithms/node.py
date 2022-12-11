f496851c0017425df8f11637cf43a8597bb70d06
Sararl27 <sararl2727@gmail.com>
2022-12-11 18:02:37 +0100

    Adding comments in node.py

--- search_algorithms/node.py
+++ search_algorithms/node.py
@@ -3,7 +3,6 @@ import util
 
 class Node:
     def __init__(self, state, parent=None, action=None, heuristic_value=0):
-        "Create a search tree Node, derived from a parent by an action."
         self.state = state
         self.parent = parent
         self.action = action
@@ -13,9 +12,11 @@ class Node:
             self.heuristic_value = heuristic_value
 
     def extend(self, problem):
+        """Return the successors nodes connected with the current node"""
         return [Node(succ, self, act, stepCost) for succ, act, stepCost in problem.getSuccessors(self.state)]
 
     def path(self):
+        """Return a list of node from the current node to the root"""
         node = self
         list_n = util.Queue()
         list_n.push(node)
@@ -25,8 +26,9 @@ class Node:
         print(list_n.list[0].action)
         return list_n.list
 
-    def inList(self, queue):
+    def inList(node, queue):
+        """Check if the node is in the list queue"""
         for n in queue:
-            if self.state == n.state:
+            if node.state == n.state:
                 return True
         return False
